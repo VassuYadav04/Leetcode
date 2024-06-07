9017856045a68077b833502f2aafe1d1f21f96a7
VassuYadav04 <vassuyadavnsj@gmail.com>
2024-06-08 02:17:26 +0530

    7 June 2024 Leetcode Daily Problem

---
+++ 7_june_24.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string replaceWords(vector<string>& dict, string sent) {
+        set<string>s;
+        for (auto i : dict)s.insert(i);
+        string ans="";
+        sent.push_back(' ');
+        string temp="";
+        for (int i=0; i<(int)sent.size(); i++) {
+            if(sent[i]==' ') {
+                ans += temp;
+                ans += (char)(' ');
+                temp = "";
+            }
+            else {
+                temp += (char)sent[i];
+                if (s.count(temp)){
+                    ans += temp;
+                    ans += char(' ');
+                    temp = "";
+                    while(sent[i]!=' ') i++;
+                }
+            }
+        }
+        ans.pop_back();
+        return ans;
+    }
+};
