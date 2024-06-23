5a2abaacbe9bf0b43ab5584ba92bc36dfc8c159f
VassuYadav04 <vassuyadavnsj@gmail.com>
2024-06-23 09:34:18 +0530

    newfile

---
+++ Interview_Top_150/274_H-Index.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        sort(citations.begin(), citations.end(), greater<int>());
+        int h = 0;
+        for (const auto& x : citations) {
+            if (x >= h + 1) {
+                ++h;
+            } else {
+                break;
+            }
+        }
+        return h;
+    }
+};
