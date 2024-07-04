8fee33af32d283681a0d2639c6fac3bf5cfebabf
VassuYadav04 <vassuyadavnsj@gmail.com>
2024-07-04 08:02:11 +0530

    newfile

---
+++ Interview_Top_150/290_Word_Pattern.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string str) {
+        int cnt = str.empty() ? 0 : 1;
+        for (const auto& c : str) {
+            if (c == ' ') {
+                ++cnt;
+            }
+        }
+        if (pattern.size() != cnt) {
+            return false;
+        }
+
+        unordered_map<string, char> w2p;
+        unordered_map<char, string> p2w;
+        int i = 0, j = 0;
+        for (const auto& p : pattern) {
+            j = str.find(" ", i);
+            if (j == string::npos) {
+                j = str.length();
+            }
+            const string w = str.substr(i, j - i);
+    
+            if (!w2p.count(w) && !p2w.count(p)) { 
+                w2p[w] = p; 
+                p2w[p] = w; 
+            } else if (!w2p.count(w) || w2p[w] != p) {
+                return false;
+            }
+            i = j + 1;
+        }
+        return true;
+    }
+};
