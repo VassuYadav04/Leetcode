58c217d95cc70d3f5331645dd5b2be83833b311d
VassuYadav04 <vassuyadavnsj@gmail.com>
2024-07-10 12:04:13 +0530

    newfile

---
+++ july/10_july_24.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        int result = 0;
+        for (const auto& log: logs) {
+            if (log == "../") {
+                if (result > 0) {
+                    --result;
+                }
+            } else if (log != "./") {
+                ++result;
+            }
+        }
+        return result;
+    }
+};
