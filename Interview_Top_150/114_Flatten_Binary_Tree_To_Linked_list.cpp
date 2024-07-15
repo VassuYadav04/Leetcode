dd602b1f78851bc207a432fb4cc80abd776b6268
VassuYadav04 <vassuyadavnsj@gmail.com>
2024-07-15 15:28:11 +0530

    newfile

---
+++ Interview_Top_150/114_Flatten_Binary_Tree_To_Linked_list.cpp
@@ -0,0 +1,21 @@
+class Solution {
+ public:
+  void flatten(TreeNode* root) {
+    if (root == nullptr)
+      return;
+
+    flatten(root->left);
+    flatten(root->right);
+
+    TreeNode* const left = root->left;
+    TreeNode* const right = root->right;
+
+    root->left = nullptr;
+    root->right = left;
+
+    TreeNode* rightmost = root;
+    while (rightmost->right)
+      rightmost = rightmost->right;
+    rightmost->right = right;
+  }
+};
