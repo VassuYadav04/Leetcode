6f3ce54b3f595e7c5c4d87f41a1121c08876149a
VassuYadav04 <vassuyadavnsj@gmail.com>
2024-07-30 15:11:53 +0530

    newfile

---
+++ july/30_july_24.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minimumDeletions(string s) {
+        int result = 0, b_cnt = 0;
+        for (const auto& c : s) {
+            if (c == 'b') {
+                ++b_cnt;
+            } else if (b_cnt) {
+                --b_cnt;
+                ++result;
+            }
+        }
+        return result;
+    }
+};
