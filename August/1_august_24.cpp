440d384d8a8795a1eaada09f495a8a18ac05ee53
VassuYadav04 <vassuyadavnsj@gmail.com>
2024-08-01 14:31:54 +0530

    newfile

---
+++ August/1_august_24.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        return accumulate(cbegin(details), cend(details), 0, [](const auto& total, const auto& x) {
+            return total + static_cast<int>(x.substr(11, 2) > "60");
+        });
+    }
+};
