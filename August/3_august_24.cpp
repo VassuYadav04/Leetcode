e54f7060d805b0a1c10821760580255833e0805a
VassuYadav04 <vassuyadavnsj@gmail.com>
2024-08-03 13:19:41 +0530

    newfile

---
+++ August/3_august_24.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canBeEqual(vector<int>& target, vector<int>& arr) {
+        return counter(target) == counter(arr);
+    }
+
+private:
+    unordered_map<int, int> counter(const vector<int>& arr) {
+        unordered_map<int, int> count;
+        for (const auto& x : arr) {
+            ++count[x];
+        }
+        return count;
+    }
+};
