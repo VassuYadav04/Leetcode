2eb00af63f071f69b7f6500801e1a65fceaccae4
VassuYadav04 <vassuyadavnsj@gmail.com>
2024-09-10 15:54:27 +0530

    newfile

---
+++ September/10_september_24.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    ListNode* insertGreatestCommonDivisors(ListNode* head) {
+        for (auto curr = head; curr->next; curr = curr->next->next) {
+            curr->next = new ListNode(gcd(curr->val, curr->next->val), curr->next);
+        }
+        return head;
+    }
+};
