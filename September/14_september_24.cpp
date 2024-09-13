43d2bcbe067a3ee0b4f946755befb88fd3c437c6
VassuYadav04 <vassuyadavnsj@gmail.com>
2024-09-13 14:27:08 +0530

    newfile

---
+++ September/14_september_24.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        const int mx = *max_element(cbegin(nums), cend(nums));
+        int result = 1, l = 0;
+        for (const auto& x : nums) {
+            if (x == mx) {
+                result = max(result, ++l);
+            } else {
+                l = 0;
+            }
+        }
+        return result;
+    }
+};
