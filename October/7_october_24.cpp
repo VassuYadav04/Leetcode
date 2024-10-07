b5b8adc1747dcd079dfa63c12f6e5b19f6a32a88
VassuYadav04 <vassuyadavnsj@gmail.com>
2024-10-07 14:48:50 +0530

    newfile

---
+++ October/7_october_24.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minLength(string s) {
+        string stk;
+        for (const auto& c : s) {
+            if (!empty(stk) && ((stk.back() == 'A' && c == 'B') || (stk.back() == 'C' && c == 'D'))) {
+                stk.pop_back();
+                continue;
+            }
+            stk.push_back(c);
+        }
+        return size(stk);
+    }
+};
