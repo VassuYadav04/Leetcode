16b28ae8246d295be842c4555bde20d686f6567a
VassuYadav04 <vassuyadavnsj@gmail.com>
2024-10-09 14:52:17 +0530

    newfile

---
+++ October/9_october_24.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minAddToMakeValid(string S) {
+        int add = 0, bal = 0;
+        for (const auto& c : S) {
+            bal += c == '(' ? 1 : -1;
+            if (bal == -1) {
+                add++;
+                bal++;
+            }
+        }
+        return add + bal;
+    }
+};
