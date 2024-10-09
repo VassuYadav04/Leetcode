389b37fb25af50ea89d44dc1541ec775dc1de909
VassuYadav04 <vassuyadavnsj@gmail.com>
2024-10-09 14:51:20 +0530

    newfile

---
+++ October/8_october_24.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minSwaps(string s) {
+        int result = 0, curr = 0;
+        for (const auto& c : s) {
+            if (c == ']') {
+               ++curr;
+                result = max(result, curr);
+            } else {
+                --curr;
+            }            
+        }
+        return (result + 1) / 2;
+    }
+};
