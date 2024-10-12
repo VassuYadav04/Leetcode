a4fdfb7286feec7f2e75ae9d3d0aa4191b1a8d43
VassuYadav04 <vassuyadavnsj@gmail.com>
2024-10-12 22:01:46 +0530

    newfile

---
+++ October/12_october_24.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minGroups(vector<vector<int>>& intervals) {
+        map<int, int> events;
+        for (const auto& i : intervals) {
+            ++events[i[0]];
+            --events[i[1] + 1];
+        }
+        int result = 0, curr = 0;
+        for (const auto& [t, d] : events) {
+            curr += d;
+            result = max(result, curr);
+        }
+        return result;
+    }
+};
