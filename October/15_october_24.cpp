ad8524d27f6a18295327563d7db29f1b879afa0d
VassuYadav04 <vassuyadavnsj@gmail.com>
2024-10-15 23:59:50 +0530

    newfile

---
+++ October/15_october_24.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    long long minimumSteps(string s) {
+        int64_t result = 0;
+        for (int right = 0, left = 0; right < size(s); ++right) {
+            if (s[right] != '0') {
+                continue;
+            }
+            result += right - left++;
+        }
+        return result;
+    }
+};
