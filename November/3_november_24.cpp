64f13d51995998f94d2b768d1f066d205454a9d2
VassuYadav04 <vassuyadavnsj@gmail.com>
2024-11-02 23:48:30 +0530

    newfile

---
+++ November/3_november_24.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    bool rotateString(string A, string B) {
+        if (A.length() != B.length()) {
+            return false;
+        }
+        return strStr(A + A, B) != -1;
+    }
+    
+private:
+    int strStr(string haystack, string needle) {
+        if (needle.empty()) {
+            return 0;
+        }
+
+        return KMP(haystack, needle);
+    }
+
+    int KMP(const string& text, const string& pattern) {
+        const vector<int> prefix = getPrefix(pattern);
+        int j = -1;
+        for (int i = 0; i < text.length(); ++i) {
+            while (j > -1 && pattern[j + 1] != text[i]) {
+                j = prefix[j];
+            }
+            if (pattern[j + 1] == text[i]) {
+                ++j;
+            }
+            if (j == pattern.length() - 1) {
+                return i - j;
+            }
+        }
+        return -1;
+    }
+
+    vector<int> getPrefix(const string& pattern) {
+        vector<int> prefix(pattern.length(), -1);
+        int j = -1;
+        for (int i = 1; i < pattern.length(); ++i) {
+            while (j > -1 && pattern[j + 1] != pattern[i]) {
+                j = prefix[j];
+            }
+            if (pattern[j + 1] == pattern[i]) {
+                ++j;
+            }
+            prefix[i] = j;
+        }
+        return prefix;
+    }
+};
