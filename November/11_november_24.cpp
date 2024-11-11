f85b62547197a101a4d99160bd7103b65c61ab52
VassuYadav04 <vassuyadavnsj@gmail.com>
2024-11-12 02:37:50 +0530

    newfile

--- November/11_november_24.cpp
+++ November/11_november_24.cpp
@@ -1,41 +1,35 @@
+vector<int> linear_sieve_of_eratosthenes(int n) {
+    vector<int> spf(n + 1, -1);
+    vector<int> primes;
+    for (int i = 2; i <= n; ++i) {
+        if (spf[i] == -1) {
+            spf[i] = i;
+            primes.emplace_back(i);
+        }
+        for (const auto& p : primes) {
+            if (i * p > n || p > spf[i]) {
+                break;
+            }
+            spf[i * p] = p;
+        }
+    }
+    return primes;
+}
+
+const int MAX_N = 1000;
+const auto& PRIMES = linear_sieve_of_eratosthenes(MAX_N - 1);
 class Solution {
 public:
-    int minimumSubarrayLength(vector<int>& nums, int k) {
-        const auto& bit_length = [](int x) {
-            return (x ? std::__lg(x) : -1) + 1;
-        };
-    
-        const int total = accumulate(cbegin(nums), cend(nums), 0, [](const auto& x, const auto& y) {
-            return x | y;
-        });
-        if (total < k) {
-            return -1;
-        }
-        vector<int> cnt(bit_length(total));
-        const auto& update = [&](int x, int d, int curr) {
-            for (int i = 0; (1 << i) <= x; ++i) {
-                if (!(x & (1 << i))) {
-                    continue;
-                }
-                if (cnt[i] == 0) {
-                    curr ^= 1 << i;
-                }
-                cnt[i] += d;
-                if (cnt[i] == 0) {
-                    curr ^= 1 << i;
-                }
+    bool primeSubOperation(vector<int>& nums) {
+        for (int i = 0; i < size(nums); ++i) {
+            const auto& it = lower_bound(cbegin(PRIMES), cend(PRIMES), i - 1 >= 0 ? nums[i] - nums[i - 1] : nums[i]);
+            if (it != cbegin(PRIMES)) {
+                nums[i] -= *prev(it);
             }
-            return curr;
-        };
-
-        int result = size(nums);
-        for (int right = 0, left = 0, curr = 0; right < size(nums); ++right) {
-            curr = update(nums[right], +1, curr);
-            for (; left <= right && curr >= k; ++left) {
-                result = min(result, right - left + 1);
-                curr = update(nums[left], -1, curr);
+            if (i - 1 >= 0 && nums[i - 1] >= nums[i]) {
+                return false;
             }
         }
-        return result;
+        return true;
     }
 };
